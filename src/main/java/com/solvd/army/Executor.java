6b69d8d91951171f8566f517c300afabbe4f8a66
Gleb Chekmezov <gleeebkachek@gmail.com>
2022-09-13 11:58:32 +0300

    DOM parser added

--- src/main/java/com/solvd/army/Executor.java
+++ src/main/java/com/solvd/army/Executor.java
@@ -1,12 +1,8 @@
 package com.solvd.army;
 
 import com.solvd.army.connection.ConnectionUtil;
-import com.solvd.army.dao.jdbc.mysql.barrack.BarrackDAO;
 import com.solvd.army.dao.jdbc.mysql.hangar.AircraftDAO;
-import com.solvd.army.dao.jdbc.mysql.jettie.BoatDAO;
-import com.solvd.army.models.barrack.Barrack;
 import com.solvd.army.models.hangar.Aircraft;
-import com.solvd.army.models.jettie.Boat;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,14 +10,49 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
+// dom parser packages
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
+import java.io.*;
+
 
 public class Executor {
 
     private static final Logger logger = LogManager.getLogger(Executor.class);
+
     public static void main(String[] args) {
           try (Connection connection = ConnectionUtil.getConnection()) {
-            logger.info(connection.getCatalog() + " – connected!");
-          } catch (SQLException e) {
+              DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+              DocumentBuilder builder = factory.newDocumentBuilder();
+              Document document = builder.parse(new File("src/main/resources/domExample.xml"));
+              document.getDocumentElement().normalize();
+
+              Element root = document.getDocumentElement();
+              logger.info(root.getNodeName()); // cars
+
+              //Get all cars
+              NodeList nList = document.getElementsByTagName("car"); // Object
+
+              for (int temp = 0; temp < nList.getLength(); temp++)
+              {
+                  Node node = nList.item(temp);
+                  if (node.getNodeType() == Node.ELEMENT_NODE)
+                  {
+                      Element eElement = (Element) node;
+                      logger.info("============================");
+                      logger.info("id = "    + eElement.getAttribute("id"));
+                      logger.info("name = "  + eElement.getElementsByTagName("name").item(0).getTextContent());
+                      logger.info("author = "   + eElement.getElementsByTagName("author").item(0).getTextContent());
+                      logger.info("location = "    + eElement.getElementsByTagName("location").item(0).getTextContent());
+                      logger.info("============================");
+                  }
+              }
+
+
+              logger.info(connection.getCatalog() + " – connected!");
+          } catch (SQLException | ParserConfigurationException |IOException | SAXException e) {
               logger.error(e);
           }
 
--- src/main/java/com/solvd/army/dao/IBaseDAO.java
+++ src/main/java/com/solvd/army/dao/IBaseDAO.java
@@ -7,15 +7,9 @@ import java.sql.Statement;
 import java.util.List;
 
 public interface IBaseDAO<T> {
-//    String DRIVER_NAME = "com.mysql.jdbc.Driver";
-//    String DB_URL = "jdbc:mysql://hostname:3306/army";
-//    String ID = "root";
-//    String PASS = "";
-
     void create(T object);
     T getById (long id);
     List<T> getAllRows();
     void update (long id);
     void remove (long id);
-
 }
