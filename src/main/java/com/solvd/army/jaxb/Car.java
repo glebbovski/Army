012a91e89a858c7b25f7b277a70e8a40bccb23ce
Gleb Chekmezov <gleeebkachek@gmail.com>
2022-09-18 00:03:38 +0300

    add jaxb task

---
+++ src/main/java/com/solvd/army/jaxb/Car.java
@@ -0,0 +1,56 @@
+package com.solvd.army.jaxb;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "vehicle")
+public class Car {
+    private String name;
+    private int year;
+    private String color;
+
+    public Car() {
+    }
+
+    public Car(String name, int year, String color) {
+        this.name = name;
+        this.year = year;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", year=" + year +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/solvd/army/jaxb/Cars.java
@@ -0,0 +1,37 @@
+package com.solvd.army.jaxb;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "vehicles")
+public class Cars {
+    @XmlElement(name = "car", type = Car.class)
+    private List<Car> cars = new ArrayList<>();
+
+    public Cars() {
+    }
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    @Override
+    public String toString() {
+        return "Cars{" +
+                "cars=" + cars +
+                '}';
+    }
+}
